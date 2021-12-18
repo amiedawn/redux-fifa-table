c3726946fc5bfc054b13e061bf63de80b9f945ef
amiedawn <amie.d.carroll@gmail.com>
2021-12-18 06:33:22 -0700

    table loading correctly

--- src/Posts.js
+++ src/Posts.js
@@ -1,33 +1,52 @@
-import { getValue } from '@testing-library/user-event/dist/utils';
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchPosts} from './action';
+import { getValue } from "@testing-library/user-event/dist/utils";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "./action";
 
 function Posts() {
   const dispatch = useDispatch();
-  const posts = useSelector( state => state);
-  
-  useEffect (() => {
-   dispatch(fetchPosts());
+  const posts = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
   }, [dispatch]);
 
   const renderPosts = () => {
     if (posts.loading) {
-      return <h1>Loading...</h1>
+      return <h1>Loading...</h1>;
       console.log("loading");
     }
-      return posts.items.map(el=> {
-        console.log("posts");
-        return <h3 key={el.fifa_id}>{el.venue}</h3>; // this is where you will set up the table
-      })
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>No. of games played at home</th>
+            <th>Percentage win at home</th>
+            <th>No. of games played away</th>
+            <th>Percentage win away</th>
+            <th>Total number of goals</th>
+          </tr>
+        </thead>
 
-  }
+        <tbody>
+          {posts &&
+            posts.items.map((el) => {
+              // return <h3 key={el.fifa_id}>{el.venue}</h3>; // this is where you will set up the table
 
-  return (
-    <div>
-      {renderPosts()}
-    </div>
-  );
-};
+              return <tr key={el.fifa_id}>
+                <td>{el.venue}</td>
+                <td>{el.location}</td>
+                <td>{el.status}</td>
+                <td>{el.time}</td>
+                <td>{el.weather.humidity}</td>
+              </tr>;
+            })}
+        </tbody>
+      </table>
+    );
+  };
+
+  return <div>{renderPosts()}</div>;
+}
 
 export default Posts;
