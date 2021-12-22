05cc0bd3caaa63bc2d1244ba1b101a3d5e77b54f
amiedawn <amie.d.carroll@gmail.com>
2021-12-21 21:21:40 -0700

    modified api

--- src/action.js
+++ src/action.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchPosts = () => async (dispatch, getState) => {
   dispatch({ type: "FETCH_POSTS_REQUEST" });
   try {
-    const response = await axios.get("http://worldcup.sfg.io/matches");
+    const response = await axios.get("http://worldcup.sfg.io/matches/");
     dispatch({ type: "FETCH_POSTS_SUCCESS", payload: response.data });
   } catch (error) {
     dispatch({ type: "FETCH_POSTS_FAILURE", error });
