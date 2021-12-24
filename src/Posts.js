1a600d50e32309b5c1dba8a85a7e2cd65df6806e
amiedawn <amie.d.carroll@gmail.com>
2021-12-23 21:40:56 -0700

    working correctly

--- src/Posts.js
+++ src/Posts.js
@@ -10,75 +10,88 @@ function Posts() {
     dispatch(fetchPosts());
   }, [dispatch]);
 
+  //initialize variables
+  let homeGames = 0;
+  let awayGames = 0;
+  let pctHomeWin = 0;
+  let pctAwayWin = 0;
+  let homeGoals = 0;
+  let awayGoals = 0;
+  let totGoals = 0;
+
   const renderPosts = () => {
     if (posts.loading) {
       return <h1>Loading...</h1>;
       console.log("loading");
     }
-    //
+    return posts.items.map((el) => {
+      {
+        homeGames = posts.items.reduce((count, el) => {
+          if (el.home_team_country === "USA") count++;
+          return count;
+        }, 0);
+      }
 
-    let homeGames = posts.items.reduce((count, el) => {
-      if (el.home_team_country === "USA") count++;
-      return count;
-    }, 0);
+      {
+        awayGames = posts.items.reduce((count, el) => {
+          if (el.away_team_country === "USA") count++;
+          return count;
+        }, 0);
+      }
+      {
+        pctHomeWin = Math.round((homeGames / 7) * 100);
+      }
+      {
+        pctAwayWin = Math.round((awayGames / 7) * 100);
+      }
+      {
+        homeGoals = posts.items.reduce((homeCount, el) => {
+          if (el.home_team_country === "USA")
+            homeCount += parseInt(el.home_team.goals);
+          return homeCount;
+        }, 0);
+      }
+      {
+        awayGoals = posts.items.reduce((awayCount, el) => {
+          if (el.away_team_country === "USA")
+            awayCount += parseInt(el.away_team.goals);
+          return awayCount;
+        }, 0);
+      }
+      {
+        totGoals = homeGoals + awayGoals;
+      }
+    });
+  }
+            
+      return (
+        <div>
+          {renderPosts()}
 
-    let awayGames = posts.items.reduce((count, el) => {
-      if (el.away_team_country === "USA") count++;
-      return count;
-    }, 0);
+          <table className="table">
+            <thead>
+              <tr>
+                <th>No. of games played at home</th>
+                <th>Percentage win at home</th>
+                <th>No. of games played away</th>
+                <th>Percentage win away</th>
+                <th>Total number of goals</th>
+              </tr>
+            </thead>
 
-    let pctHomeWin = Math.round((homeGames / 7) * 100);
-    let pctAwayWin = Math.round((awayGames / 7) * 100);
-
-    let homeGoals = posts.items.reduce((homeCount, el) => {
-      if (el.home_team_country === "USA")
-        homeCount += parseInt(el.home_team.goals);
-      return homeCount;
-    }, 0);
-
-    let awayGoals = posts.items.reduce((awayCount, el) => {
-      if (el.away_team_country === "USA")
-        awayCount += parseInt(el.away_team.goals);
-      return awayCount;
-    }, 0);
-
-    let totGoals = homeGoals + awayGoals;
-
-    // return value.items.map(el => {})
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>No. of games played at home</th>
-            <th>Percentage win at home</th>
-            <th>No. of games played away</th>
-            <th>Percentage win away</th>
-            <th>Total number of goals</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {posts &&
-            posts.items.map((el) => {
-              // this is where you will set up the table
-              return (
-                <tr key={el.fifa_id}>
-                  <td>{homeGames}</td>
-                  <td>{pctHomeWin}</td>
-                  <td>{awayGames}</td>
-                  <td>{pctAwayWin}</td>
-                  <td>{totGoals}</td>
-                </tr>
-              );
-            })}
-          
-
-        </tbody>
-      </table>
-    );
+            <tbody>
+              <tr>
+                <td>{homeGames}</td>
+                <td>{pctHomeWin}</td>
+                <td>{awayGames}</td>
+                <td>{pctAwayWin}</td>
+                <td>{totGoals}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
   };
 
-  return <div>{renderPosts()}</div>;
-};
 
 export default Posts;
